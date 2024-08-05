98fcbdade7ecc2e49ddf4c618aa98396bab0afb5
armandasuryan <armanda.surya97@gmail.com>
2024-08-05 17:30:36 +0700

    add setting api for upload file to minio

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,9 +5,9 @@ import { auth } from "../middleware/jwtAuth.js";
 const userRoutes = express.Router();
 
 userRoutes.post('/login', userController.userLogin);
-userRoutes.post('/user/create', [auth], userController.createUserData)
-userRoutes.put('/user/update', [auth], userController.updateUserData)
-userRoutes.delete('/user/delete', [auth], userController.deletedUserData)
-userRoutes.get('/user/list', [auth], userController.getListUserData)
+userRoutes.post('/user/create', [auth], userController.createUserData);
+userRoutes.put('/user/update', [auth], userController.updateUserData);
+userRoutes.delete('/user/delete', [auth], userController.deletedUserData);
+userRoutes.get('/user/list', [auth], userController.getListUserData);
 
 export default userRoutes;
